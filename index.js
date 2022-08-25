ba7e636b92699b43ed000d09a83e832a278f5044
Prakhar Sharma <89513841+prakhhaar@users.noreply.github.com>
2022-08-25 17:42:19 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,91 @@
+// LessonOne Assignment
+var readlineSync = require("readline-sync");
+
+var score = 0;
+
+console.log("Welcome to football quiz");
+console.log("--------------------")
+
+// Function to check answers and update score
+function checkAnswers(questions) {
+  for (var i = 0; i < questionsList.length; i++) {
+    var userAnswer = readlineSync.question(questions[i].question);
+    if (userAnswer == questionsList[i].answer) {
+      score++;
+      console.log("--------------------");
+    } else {
+      console.log("--------------------");
+    }
+  }
+}
+
+// Objects of questions
+var q1 = {
+  question: "How many pennalties are usually taken in a penalty shoot-out? ",
+  answer: 5
+};
+
+var q2 = {
+  question: "Which football club did David Beckham played for? ",
+  answer: "Real Madrid"
+};
+
+var q3 = {
+  question: "Who won the 1998 Football World Cup Final? ",
+  answer: "France"
+};
+
+var q4 = {
+  question: "What is the main color of Brazil's national team shirt? ",
+  answer: "Yellow"
+};
+
+var q5 = {
+  question: "How many minutes are usually played in a football match? ",
+  answer: 90
+};
+
+var q6 = {
+  question: "Where were the 2002 World Cup Finals held? ",
+  answer: "Japan and Korea"
+};
+
+var q7 = {
+  question: "Celtic and Rangers plays their football in which Scottish city? ",
+  answer: "Glasgow"
+};
+
+var q8 = {
+  question: "What color cards does refrees carry in a football match? ",
+  answer: "Red and Yellow"
+}
+
+var q9 = {
+  question: "Real Madrid plays football in which league? ",
+  answer: "LaLiga"
+};
+
+var q10 = {
+  question: "Which English football club has the nickname 'The Gunners'? ",
+  answer: "Arsenal"
+}
+
+// Array of questions
+var questionsList = [q1, q2, q3, q4, q5];
+var questionsListTwo = [q6, q7, q8, q9, q10];
+checkAnswers(questionsList);
+
+if (score == questionsList.length) {
+  console.log("Your score is: " + score + "/" + questionsList.length);
+  console.log("Congratulations! You've advanced to next level.");
+  console.log("--------------------")
+
+  var score = 0;
+  checkAnswers(questionsListTwo);
+  var totalQuestions = questionsList.length + questionsListTwo.length;
+  var finalScore = questionsList.length + score;
+  console.log("Your score is: " + finalScore + "/" + totalQuestions);
+
+} else {
+  console.log("Your score is: " + score + "/" + questionsList.length);
+}
